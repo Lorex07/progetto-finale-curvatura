18c09ef73e902355281d77e18217edec0610fb5a
Lorenzo De Santis <110561736+Lorex07@users.noreply.github.com>
2023-05-05 13:28:31 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,24 +7,61 @@
 
 **/
 
-function preload() {
-  treeModel = loadAsset("albero.glb");
-}
-
 function setup() {
   createCanvas3D(windowWidth, windowHeight);
-  
-  //Change background color of the scene
-  background3D("#333333");
-  
-  hideGrid();
-  
-  var tree = clone(treeModel,0,0,0);
+  background3D("#99FFFF");
 
-  align(BOTTOM);
-  diffuse("grey");
-  box(0, 0, 0, 6, 0.5, 6);
+  environment(SUNSET);
+  
+  
+//ALBERO
+var albero = beginGroup();
+diffuse("#499200");
+capsule(0, 3, 0, 2);
+align (TOP);
+diffuse("#994C00");
+  cylinder(0, 0, 0, 0.5, 1.5);
+  endGroup();
+  
+  //CESPUGLIO
+  var cespuglio = beginGroup();
+  diffuse("#499200");
+  sphere(0, -1, 0, 1.3)
+  endGroup();
+  
+  
+  var lato = 110;
+  var lato2 = 10;
+  var numeroAlberi = 90
+  for (var i=0; i<numeroAlberi; i++){
+    
+    var x = random(-lato/2, lato/2);
+    var y = 0;
+    var z = random(-lato/2, lato/2);
+    
+   var nuovoalbero = clone(albero, x, y, z);
+    nuovoalbero.setScale(random(1, 2));
+    
+    var x = random(-lato/2, lato/2);
+    var y = 0;
+    var z= random(-lato/2, lato/2);
+    clone(cespuglio, x, y, z);
+  }
+  align(BOTTOM)
+  diffuse("#458801");
+  box(0,0,0, lato,1, lato)
+  
+  pushFX(BLOOM, 0.13);
+  
+
 }
+function draw(){
+  
+  
+  
+  
+}
+
 
 function draw() {
   // 🔍 Never create objects here, but don't forget to have it in your code:
